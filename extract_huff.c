b2e52c0029f0667573bf07e2a95708b0479c2bca
Shchurov Alexey <alexiskhb@gmail.com>
2015-07-14 09:49:36 +1000

    now norm adding nodes

--- extract_huff.c
+++ extract_huff.c
@@ -12,20 +12,12 @@ typedef
 static huff_node_t * find_parent(huff_node_t *parent, char bit)
 {
 	if (bit == '0')
-	{
-		if (parent->left)
-			return find_parent(parent->left, bit);
-		else
-			return parent;		
-	}
-	else if (bit == '1')
-	{
-		if (parent->right)
-			return find_parent(parent->right, bit);
-		else
-			return parent;
-	}
-	else return NULL;
+		return parent->left ? find_parent(parent->left, bit) : parent;
+	else 
+	if (bit == '1')
+		return parent->right ? find_parent(parent->right, bit) : parent;
+	else 
+		return NULL;
 }
 
 static huff_node_t * create_huff_node(const char c)
@@ -36,13 +28,14 @@ static huff_node_t * create_huff_node(const char c)
 	return new_node;
 }
 
-static void add_huff_node(huff_node_t *root, char bit, char c)
+static huff_node_t * add_huff_node(huff_node_t *root, char bit, char c)
 {
-	huff_node_t *parent = find_parent(root, bit);
+	huff_node_t *parent = find_parent(root, bit), *result;
 	if (bit == '0')
-		parent->left = create_huff_node(c);
+		result = parent->left ? parent->left : parent->left = create_huff_node(c);
 	else if (bit == '1')
-		parent->right = create_huff_node(c);
+		result = parent->right ? parent->right : parent->right = create_huff_node(c);
+	return result;
 }
 
 extern void extract_huffman(FILE *fin)
@@ -55,13 +48,16 @@ extern void extract_huffman(FILE *fin)
 	root->c = (char)0;
 	int key_leng = 1, i;
 	char c, bit;
+	huff_node_t *cur_node = root;
 	while (key_leng != 0)
 	{
+		cur_node = root;
 		fscanf(fin, "%d %c", &key_leng, &c);
 		for(i = 0; i < key_leng; i++)
 		{
 			fscanf(fin, "%c", &bit);
-			add_huff_node(root, bit, c);			
+			cur_node = add_huff_node(cur_node, bit, c);			
 		}
-	}		
+	}
+		
 }
