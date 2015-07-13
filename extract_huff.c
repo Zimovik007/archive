9c6273a9d63b06d0dd69b1113455349ad1b3e045
Shchurov Alexey <alexiskhb@gmail.com>
2015-07-14 09:34:12 +1000

    +add extract huff node

--- extract_huff.c
+++ extract_huff.c
@@ -9,16 +9,40 @@ typedef
 		struct huff_node_t *left, *right;
 	} huff_node_t;
 	
-static void find(huff_node_t *parent, char bit)
+static huff_node_t * find_parent(huff_node_t *parent, char bit)
 {
-	
-	
+	if (bit == '0')
+	{
+		if (parent->left)
+			return find_parent(parent->left, bit);
+		else
+			return parent;		
+	}
+	else if (bit == '1')
+	{
+		if (parent->right)
+			return find_parent(parent->right, bit);
+		else
+			return parent;
+	}
+	else return NULL;
+}
+
+static huff_node_t * create_huff_node(const char c)
+{
+	huff_node_t *new_node = (huff_node_t*)malloc(sizeof(huff_node_t));
+	new_node->c = c;
+	new_node->left = new_node->right = NULL;
+	return new_node;
 }
 
 static void add_huff_node(huff_node_t *root, char bit, char c)
 {
-	
-	
+	huff_node_t *parent = find_parent(root, bit);
+	if (bit == '0')
+		parent->left = create_huff_node(c);
+	else if (bit == '1')
+		parent->right = create_huff_node(c);
 }
 
 extern void extract_huffman(FILE *fin)
@@ -26,7 +50,7 @@ extern void extract_huffman(FILE *fin)
 	double b_count;
 	fscanf(fin, "%lf\n", &b_count);
 	long unsigned int bits_count = (int)b_count;
-	huff_node_t *root = (huff_node_t)malloc(sizeof(huff_node_t));
+	huff_node_t *root = (huff_node_t*)malloc(sizeof(huff_node_t));
 	root->left = root->right = NULL;
 	root->c = (char)0;
 	int key_leng = 1, i;
