76d4ff5d7adf9d5c449962be144b2af0d8fbb7ea
Shchurov Alexey <alexiskhb@gmail.com>
2015-07-13 19:58:07 +1000

    + queue_pop

--- priority_queue.c
+++ priority_queue.c
@@ -41,12 +41,39 @@ void queue_insert(queue_t *que, int priority, void *element)
 {
 	int count = ++que->count, *priors = que->priority;
 	void **els = que->elements;
-	que->priority = (int*)realloc(priors, count*sizeof(int));
-	que->elements = (void**)realloc(els, count*sizeof(void*));
-	priors[count - 1] = priority;
-	els[count - 1] = element;
-	queue_sift_up(que, count - 1);
+	que->priority = (int*)realloc(priors, (count + 1)*sizeof(int));
+	que->elements = (void**)realloc(els, (count + 1)*sizeof(void*));
+	priors[count] = priority;
+	els[count] = element;
+	queue_sift_up(que, count);
 }
 
-//void 
+static void queue_sift_down(queue_t *que)
+{
+	int i, *priors = que->priority, min_ch_pr = 1, count = que->count;
+	for(i = 1; ; i = min_ch_pr)
+	{
+		if (2*i + 1 >= count)
+			min_ch_pr = priors[2*i] < priors[2*i + 1] ? 2*i : 2*i + 1;
+		else if (2*i == count)
+			min_ch_pr = 2*i;
+		else return;
+		
+		if (priors[i] > priors[min_ch_pr])
+			swap_queue_elements(que, i, min_ch_pr);
+		else break;		
+	}
+}
+
+void * queue_pop(queue_t *que)
+{
+	int count = --que->count, *priors = que->priority;
+	void **els = que->elements, *result = que->elements[1];
+	que->elements[1] = els[count + 1];
+	que->priority[1] = priors[count + 1];
+	que->elements = realloc(els, (count + 1)*sizeof(void*));
+	que->priority = (int*)realloc(priors, (count + 1)*sizeof(int));
+	queue_sift_down(que);
+	return result;	
+}
 
