d5f1dd4fbae39a04c3b00bd56a70cfc3a088b01f
Zimovik007 <zimovik@list.ru>
2015-07-13 13:34:24 +1000

    Create main.c

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void compress(FILE *fin, char ArchiveName[200]);
+void extract(FILE *fin, char FileName[200]);
+
+int main()
+{
+    FILE *fin, *fout;
+    printf("Hello, User.\n It's archivator develompment:\n   Gusarov V., Schyrov Al., Karandaev T.\n");
+    printf("Let's start: ");
+    int i, j;
+    char address[200];
+    char FileName[200];
+    char ArchiveName[200];
+    for(i = 0; i < 200; i++){
+      FileName[i] = NULL;
+      ArchiveName[i] = NULL;
+    }
+    //Работа с консолью
+    if (strcmp(address, "exit") == 0) return 0;
+    while(1){
+      printf("\n             ");
+      gets(address);
+      if (strcmp(address, "exit") == 0) return 0;
+      i = 0;
+      while (1){
+        if (address[i] == ' ') break;
+        FileName[i] = address[i];
+        i++;
+      }
+      i++;
+      j = 0;
+      while (1){
+        if (address[i] == ' ') break;
+        ArchiveName[j] = address[i];
+        i++; j++;
+      }
+      i += 2;
+      if (address[i] == 'a'){
+        fin = fopen(FileName, "rb");
+        compress(fin, ArchiveName);
+        break;
+      } else if (address[i] == 'e'){
+          fin = fopen(ArchiveName, "rb");
+          extract(fin, FileName);
+          break;
+        } else{
+            printf("\nUnknowError, Try again: ");
+          }
+    }
+
+    return 0;
+}
+
+void compress(FILE *fin, char ArchiveName[200]){
+
+}
+
+void extract(FILE *fin, char FileName[200]){
+
+}
+
