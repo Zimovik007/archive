b505523b9d793e12dbab45140c5d5519c500fa8f
Zimovik007 <zimovik@list.ru>
2015-07-15 10:03:45 +1000

    extract func
    Считывание из архива по схеме

--- main.c
+++ main.c
@@ -2,20 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "compress.c"
-#include "extract_huff.c"
+//#include "extract_huff.c"
 
-void compress(FILE *fin, char ArchiveName[200]);
+void compress(FILE *fin, char ArchiveName[200], int fileCount);
 void extract(FILE *fin, char FileName[200]);
 
 int main(int argc, char* argv[]){
-  FILE *fin;
-  fin = fopen("input.jpg", "r");
-  compress_huffman(fin);
-  fclose(fin);
-  return 0;
-  
-  int i;
-  //char address[200];
+  FILE *fin, *fout;
+  int i, j;
+  char c;
+  char address[200];
   char FileName[200];
   char ArchiveName[200];
   for(i = 0; i < 200; i++){
@@ -28,20 +24,35 @@ int main(int argc, char* argv[]){
     return 0;
   }
   if (argc == 1){
-    printf("\nNameFile ArchiveFile -a  -> Archive");
-    printf("\nNameFile ArchiveFile -e  -> Extract");
+    printf("\n\nHello, bro. It's archivator. Developers: \n");
+    printf("    Gusarov.v, Schyrov Al., Karandaev T.\n\n==================\n\n");
+    printf("Look scheme comands:");
+    printf("\n  [FileName] [ArchiveName] -a    |Archive");
+    printf("\n  [FileName] [ArchiveName] -e    |Extract\n\n==================\n");
     return 0;
   }
-  if (strcmp(argv[3], "-a") == 0){
-    if (fopen(argv[1], "rb") != NULL){
-      fin = fopen(argv[1], "rb");
-      compress(fin, argv[2]);
+    i = 1;
+    int* lengths = malloc(sizeof(int) * (argc - 2));
+    fout = fopen("temp.txt", "w");
+    while (i < argc - 2){
+      fin = fopen(argv[i], "r");
+      while (fscanf(fin, "%c", &c) != EOF){
+        lengths[i]++;
+        fprintf(fout, "%c", c);
+      }
+      i++;
+    }
+
+  if (strcmp(argv[argc - 1], "-a") == 0){
+    if (fopen("temp.txt", "rb") != NULL){
+      fin = fopen("temp.txt", "rb");
+      compress(fin, argv[argc - 2], argc-3);
     } else
         printf("\nCannot find file: %s ", argv[1]);
-  } else if (strcmp(argv[3], "-e") == 0){
-      if (fopen(argv[2], "rb") != NULL){
-        fin = fopen(argv[2], "rb");
-        extract(fin, argv[1]);
+  } else if (strcmp(argv[argc - 1], "-e") == 0){
+      if (fopen(argv[argc - 2], "rb") != NULL){
+        fin = fopen(argv[argc - 2], "rb");
+        extract(fin, argv[argc - 3]);
       } else
           printf("\nCannot find file: %s ", argv[2]);
     } else
@@ -50,30 +61,38 @@ int main(int argc, char* argv[]){
   return 0;
 }
 
-void compress(FILE *fin, char ArchiveName[200]){
-  char c;
-  FILE *fout;
-  fout = fopen(strncat(ArchiveName, ".vlt", 4), "w");
-  fprintf(fout, "vlt\n");
-  while (1){
-    c = fgetc(fin);
-    if (c == EOF) break;
-    fprintf(fout, "%c", c);
-  }
+void compress(FILE *fin, char ArchiveName[200], int fileCount){
+  compress_huffman(fin, ArchiveName, fileCount);
 }
 
 void extract(FILE *fin, char FileName[200]){
-  char c;
-  FILE *fout;
-  fout = fopen(FileName, "w");
-  char test[4];
-  fgets(test, 4, fin);
-  if (strcmp(test, "vlt") == 0){
-    while (1){
-      c = fgetc(fin);
-      if (c == EOF) break;
-      fprintf(fout, "%c", c);
-    }
-  } else printf("Error... Wrong Format File...");
+  int i, j;
+  char test1[17], test2[10], test3;
+  fgets(test1, 17, fin);
+  fscanf(fin, "\n");
+  fgets(test2, 10, fin);
+  fscanf(fin, "\n");
+  if (strcmp(test1, "UPA file archive") != 0){printf("Wrong format file..."); return;}
+  if (strcmp(test2, "sign: UPA") != 0) {printf("Wrong format file..."); return;}
+  char alg_id[5];
+  fgets(alg_id, 5, fin);
+  fscanf(fin, "\n");
+  fscanf(fin, "%c\n", &test3);
+  if (test3 != '1') {printf("Wrong type extract... Sorry"); return;}
+  char FileCount;
+  fscanf(fin, "%c", &FileCount);
+  char** FileNames;
+  char* FileLenghts;
+  FileNames = malloc(sizeof(char) * FileCount);
+  FileLenghts = malloc(sizeof(char) * FileCount);
+  for(i = 0; i < FileCount; i++){
+    fscanf(fin, "%c\n", &FileLenghts[i]);
+    FileNames[i] = malloc(sizeof(char) * FileLenghts[i]);
+    for(j = 0; j < FileLenghts[i]; j++)
+      fscanf(fin, "%c", &FileNames[i][j]);
+    fscanf(fin, "\n");
+  }
+  return;
+  //if (alg_id == "HUFF") extract_huffman(fin);
 }
 
