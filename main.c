d678a5e8428fadfd308edf46b7e2148b1bcd6ab2
Zimovik007 <zimovik@list.ru>
2015-07-17 14:40:14 +1000

    Release v.2
    Все работает по канону

--- main.c
+++ main.c
@@ -24,11 +24,20 @@ void  Archive                  (char* argv[], int argc);
 void  Extract                  ();
 int   get_archive_file_names   (char* argv[], int argc);
 char  print_solid_count        (char* argv[], int argc, int count);
-void  print_FAT                (int count);
+void  print_FAT                (int count, char c);
 void  prepare_files            (char c, int count, char* argv[]);
 void  print_compress_data      (char c, int count);
 char* read_signature_files     (FILE *fin, int *FileCoint, int *BufLen, int *d);
 void  extract                  (char* alg_id, int d, FILE *fin, int FileCount, int BufLen);
+void  print_bin_header         (FILE *fout, int files_count, int is_solid);
+void  print_bin_fat_entry      (FILE *fout, int fname_len, char *fname, int packed_size, int orig_size, int is_solid);
+int   f_is_upa                 (FILE *fin);
+int   f_is_solid               (FILE *fin);
+int   f_fname_len              (FILE *fin);
+char* f_fname                  (FILE *fin, int fname_len);
+void  f_read_attributes        (FILE *fin);
+
+unsigned long long int f_int_read(FILE *fin, int size);
 
 char*  ArchiveName;
 char** FileNames;
@@ -72,12 +81,11 @@ int main(int argc, char* argv[]){
   //Если отсутствует ключ то мы ищем сигнатуру в первом файле
   if (c == 0){
     temp = fopen(argv[1], "rb");
-    fgets(test1, 4, temp);
-    fclose(temp);
-    if (strcmp(test1, "UPA") == 0){
+    if (f_is_upa(temp)){
       ArchiveName = argv[1];
       Extract();
     } else Archive(argv, argc);
+    fclose(temp);
   }
   return 0;
 }
@@ -98,7 +106,7 @@ void Archive(char* argv[], int argc){
   c = print_solid_count(argv, argc, count);
   Files = malloc(sizeof(entry) * count);
   prepare_files(c, count, argv);
-  print_FAT(count);
+  print_FAT(count, c);
   print_compress_data(c, count);
 
   t = time(NULL);
@@ -126,15 +134,13 @@ int get_archive_file_names(char* argv[], int argc){
 
 char print_solid_count(char* argv[], int argc, int count){
   int i;
-  char c = (int)1;
+  char c = 1;
   fout = fopen(ArchiveName, "wb");
-  fprintf(fout, "UPA\nHUFF\n");
   for (i = count+1; i < argc; i++){
     if (strcmp(argv[i], "-solid") == 0){c = 1; break;}
     if (strcmp(argv[i], "-nosolid") == 0){c = 0; break;}
   }
-  fprintf(fout, "%d\n", (int)c);
-  fprintf(fout, "%d\n", count);
+  print_bin_header(fout, count, c);
   return c;
 }
 
@@ -178,16 +184,13 @@ void prepare_files(char c, int count, char* argv[]){
   }
 }
 
-void print_FAT(int count){
+void print_FAT(int count, char c){
   int i;
   for(i = 0; i < count; i++){
-    fprintf(fout, "%d\n", Files[i].FileLength);
-    fprintf(fout, "%s\n", Files[i].FileName);
-    fprintf(fout, "%d\n", Files[i].SizePacked);
-    fprintf(fout, "%d\n", Files[i].SizeOriginal);
-    fprintf(fout, "%d\n", Files[i].Attributes);
-    fprintf(fout, "%d\n", Files[i].Time_Created);
-    fprintf(fout, "%d\n", Files[i].Time_Modified);
+    print_bin_fat_entry(fout, Files[i].FileLength,
+                              Files[i].FileName,
+                              Files[i].SizePacked,
+                              Files[i].SizeOriginal, c);
   }
 }
 
@@ -237,27 +240,29 @@ void Extract(){
 }
 
 char* read_signature_files(FILE *fin, int* FileCount, int* BufLen, int* d){
-  int   i;
-  char  test1[4];
+  int   i, sign;
+  char  test1[5];
   char* alg_id;
-  alg_id = malloc(sizeof(char) * 5);
-  fgets(test1, 4, fin); fscanf(fin, "\n");
-  fgets(alg_id, 5, fin); fscanf(fin, "\n");
-  fscanf(fin, "%d", d);
-  if (strcmp(test1, "UPA") != 0){printf("Wrong Format File!\n"); exit(0);}
-  fscanf(fin,"%d\n", FileCount);
+  alg_id = malloc(sizeof(char) * 4);
+
+  sign = f_is_upa(fin);
+  fgets(alg_id, 5, fin);
+  *d = f_is_solid(fin);
+  if (sign != 1){ printf("Wrong Format File!\n"); exit(0); }
+  *FileCount = f_int_read(fin, 2);
   Files = malloc(sizeof(entry) * (*FileCount));
+
   for(i = 0; i < *FileCount; i++){
-    fscanf(fin, "%d\n", &Files[i].FileLength);
+    Files[i].FileLength = f_fname_len(fin);
     Files[i].FileName = malloc(sizeof(char) * Files[i].FileLength);
-    fscanf(fin, "%s", Files[i].FileName);
-    fscanf(fin, "%d\n", &Files[i].SizePacked);
-    fscanf(fin, "%d\n", &Files[i].SizeOriginal);
-    fscanf(fin, "%d\n", &Files[i].Attributes);
-    fscanf(fin, "%d\n", &Files[i].Time_Created);
-    fscanf(fin, "%d\n", &Files[i].Time_Modified);
+    Files[i].FileName = f_fname(fin, Files[i].FileLength);
+    Files[i].SizePacked = f_int_read(fin, 8);
+    Files[i].SizeOriginal = f_int_read(fin, 8);
+    f_read_attributes(fin);
     *BufLen += Files[i].SizeOriginal;
   }
+
+
   return alg_id;
 }
 
@@ -265,6 +270,7 @@ void extract(char* alg_id, int d, FILE *fin, int FileCount, int BufLen){
   int  i, j;
   char c;
   FILE *temp, *temp2;
+  printf("%s\n", alg_id);
   if (strcmp(alg_id, "HUFF") == 0){
     if (d == 0){
       for (i = 0; i < FileCount; i++){
@@ -294,3 +300,95 @@ void extract(char* alg_id, int d, FILE *fin, int FileCount, int BufLen){
   }
   return;
 }
+
+
+void print_bin_header(FILE *fout, int files_count, int is_solid){
+  fprintf(fout, "UPAHUFF%c", is_solid);
+  int i, j, bit;
+  unsigned short int f_count = (unsigned short int)files_count;
+  for(i = 1; i >= 0; i--){
+    char c = 0;
+    for(j = 7; j >= 0; j--){
+      bit = !!(f_count & (1 << (i*8 + j)));
+  	  c = c | (bit << j);
+    }
+  fprintf(fout, "%c", c);
+  }
+}
+
+void print_bin_fat_entry(FILE *fout, int fname_len, char *fname, int packed_size, int orig_size, int is_solid)
+{
+	int i, j, bit;
+	unsigned long long int p_size = (unsigned long long int)packed_size;
+	unsigned long long int o_size = (unsigned long long int)orig_size;
+	fprintf(fout, "%c", (unsigned char)fname_len);
+	for(i = 0; i < fname_len; i++)
+		fprintf(fout, "%c", fname[i]);
+	//packed size
+	for(i = 7; i >= 0; i--){
+		char c = 0;
+		for(j = 7; j >= 0 && !is_solid; j--){
+			bit = !!(p_size & (1 << (i*8 + j)));
+			c = c | (bit << j);
+		}
+		fprintf(fout, "%c", c);
+	}
+	//orig_size
+	for(i = 7; i >= 0; i--){
+		char c = 0;
+		for(j = 7; j >= 0; j--){
+			bit = !!(o_size & (1 << (i*8 + j)));
+			c = c | (bit << j);
+		}
+		fprintf(fout, "%c", c);
+	}
+	fprintf(fout, "%c%c%c%c%c", 0, 0, 0, 0, 0);
+}
+
+
+int f_is_upa(FILE *fin){
+  char u, p, a, c;
+  fscanf(fin, "%c%c%c", &u, &p, &a);
+  return u == 'U' && p == 'P' && a == 'A';
+}
+
+int f_is_solid(FILE *fin){
+  char c;
+  fscanf(fin, "%c", &c);
+  return (int)c;
+}
+
+int f_fname_len(FILE *fin){
+  unsigned char c;
+  fscanf(fin, "%c", &c);
+  return (int)c;
+}
+
+char* f_fname(FILE *fin, int fname_len){
+  int i;
+  char *fname = (char*)malloc(fname_len * sizeof(char));
+  for(i = 0; i < fname_len; i++)
+    fscanf(fin, "%c", &fname[i]);
+  return fname;
+}
+
+unsigned long long int f_int_read(FILE *fin, int size){
+  unsigned long long int res = 0, bit;
+  int i, j;
+  char c;
+  for(i = size - 1; i >= 0; i--){
+    fscanf(fin, "%c", &c);
+    for(j = 7; j >= 0; j--){
+      bit = !!(c & (1 << j));
+      res = res | (bit << (i*8 + j));
+    }
+  }
+  return res;
+}
+
+void f_read_attributes(FILE *fin){
+  char c;
+  fscanf(fin, "%c%c%c%c%c", &c, &c, &c, &c, &c);
+}
+
+
